17c64374fed63b2f8d80791537bd47b3d72e3903
binohub <yeebino001@gmail.com>
2022-07-13 13:42:44 +0200

    api - start

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("ehy piccolo dev! sono main.js 😛 ora puoi usarmi! usami uwu ")
+// ! vue
+const app = new Vue({
+    el: '#app',
+    data: {
+        mail: "",
+    },
+    methods: {
+
+    },
+
+    created(){
+
+        // .then( (response) => ) {
+
+        // }
+    },
+})
