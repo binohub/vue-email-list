9c091a2750c45e945bf0275e1508ee9df9f3ae0f
binohub <yeebino001@gmail.com>
2022-07-13 14:15:48 +0200

    api - just one random mail generator - added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 console.log("ehy piccolo dev! sono main.js 😛 ora puoi usarmi! usami uwu ")
 // ! vue
-const app = new Vue({
-    el: '#app',
-    data: {
-        mail: "",
-    },
-    methods: {
+const app = new Vue(
+    {
+        el: '#app',
 
-    },
+        data: {
+            mail: "",
+        },
+        methods: {
 
-    created(){
 
-        // .then( (response) => ) {
+        },
 
-        // }
-    },
-})
+        created(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (response) => {
+                console.log(response);
+                console.log(`axios ON! 😀`);
+                console.log(response.data);
+                console.log("random mail ON! 😀 ");
+                console.log("<< CONNETTI response.data.response A mail >>");
+
+                this.mail = response.data.response;
+
+                console.log("connected! 😀");
+            });
+        },
+
+    });
