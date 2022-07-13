eb73b813fb38ab2ddfce9b383f6a7fff5518b37d
binohub <yeebino001@gmail.com>
2022-07-13 14:54:02 +0200

    api - counter & push - added

--- js/script.js
+++ js/script.js
@@ -6,25 +6,37 @@ const app = new Vue(
 
         data: {
             mail: "",
+            mails: [],
         },
         methods: {
+            mailGenerator(){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (response) => {
+                    // console.log(response);
+                    // console.log(`axios ON! 😀`);
+                    // console.log(response.data);
+                    // console.log("random mail ON! 😀 ");
+  
+                    const result = response.data.response;
+                    console.log("mail generated! i'm pushing in the array... 😀");
+                    this.mails.push(result);
+                    console.log(result);
+                });
+            },
+
+            counter(){
+                for(let index=0; index <= 9; index ++){
+                    this.mailGenerator();
+                }
+                console.log(this.mails);
+            },
+            
 
 
         },
 
         created(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then( (response) => {
-                console.log(response);
-                console.log(`axios ON! 😀`);
-                console.log(response.data);
-                console.log("random mail ON! 😀 ");
-                console.log("<< CONNETTI response.data.response A mail >>");
-
-                this.mail = response.data.response;
-
-                console.log("connected! 😀");
-            });
+            this.counter();
         },
 
     });
